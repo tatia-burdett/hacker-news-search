b56711fb4243457d60681b9a4abd8df62e78ca96
Tatia burdett <tatiaburdett@gmail.com>
2021-06-03 14:04:38 -0700

    add react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
+// Build an application that has two pages for users to go two
+  // /search -- which lets the users search the Hacker News Algolia API + dislay results
+  // /history -- shows a list of the users past searches from this session (refresh doesn't need to keep data)
+
+// React router for pages
+// Store fetch request in state in app
+
 function App() {
   return (
     <main className='App'>
